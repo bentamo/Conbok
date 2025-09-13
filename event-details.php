b84fc9a4fbf223beba0e3e80fddbc567ab3ea7c4
Sean Montecillo <montecillomikhail@gmail.com>
2025-09-13 08:48:43 +0800

    ADD: Added some new css styling for better output showing

--- event-details.php
+++ event-details.php
@@ -239,26 +239,26 @@ add_shortcode('event-image', 'conbook_event_image_shortcode');
 // Shortcode: [event-description]
 // Uses GET parameter 'event-slug'
 // -------------------------------
+// -------------------------------
+// Shortcode: [event-description]
+// Uses GET parameter 'event-slug'
+// -------------------------------
 function conbook_event_description_shortcode($atts) {
-    // Get the event slug from URL ?event-slug=
     $slug = sanitize_text_field($_GET['event-slug'] ?? '');
     if (!$slug) return '';
 
-    // Get the event by slug
     $event = get_page_by_path($slug, OBJECT, 'event');
     if (!$event) return '';
 
-    $post_id = $event->ID;
-
-    // Get the post content
-    $content = apply_filters('the_content', $event->post_content);
+    $content = wpautop($event->post_content);
     if (!$content) return '';
 
-    $output = '<div class="event-description"><center>';
-    $output .= '<h3>Description</h3>'. $content;
-    $output .= '</center></div>';
+    // Wrap content without interfering with <p> tags
+    $output = '<section class="event-description">';
+    $output .= '<h3 class="event-description-title">Description</h3>';
+    $output .= '<div class="event-description-content">' . $content . '</div>';
+    $output .= '</section>';
 
     return $output;
 }
 add_shortcode('event-description', 'conbook_event_description_shortcode');
-?>
