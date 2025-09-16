27324ca202c35b171bc4e268147350a1627022e3
Jess Adrian Ura-ura <20220024319@my.xu.edu.ph>
2025-09-16 15:09:49 +0800

    PATCH: Restricted the Event Dashboard to Event Organizer Only

--- event-dashboard.php
+++ event-dashboard.php
@@ -2,8 +2,27 @@
 // -------------------------------
 // Shortcode: [event-dashboard]
 // Displays a custom event dashboard with centered clickable tabs (no background)
+// Restricted so only the event's author can see it
 // -------------------------------
 function conbook_event_dashboard_shortcode($atts) {
+    global $post;
+
+    // Bail if not inside a post context
+    if ( ! $post ) {
+        return '';
+    }
+
+    // Get current user ID
+    $current_user_id = get_current_user_id();
+
+    // Get post author ID
+    $author_id = (int) $post->post_author;
+
+    // Restrict: only author can see the dashboard
+    if ( $current_user_id !== $author_id ) {
+        return 'You are not allowed to view this dashboard.';
+    }
+
     // Start output buffering
     ob_start();
     ?>
@@ -76,4 +95,3 @@ function conbook_event_dashboard_shortcode($atts) {
     return ob_get_clean();
 }
 add_shortcode('event-dashboard', 'conbook_event_dashboard_shortcode');
-?>
